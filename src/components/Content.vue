4815b161470a3c0457b71d68b292910863779474
AXZET-5 <51935353+AXZET-5@users.noreply.github.com>
2023-05-04 22:35:11 +0800

    Update Content.vue

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,9 +1,50 @@
+<!--
 <script setup>
   import IA from './IndivAd.vue'
   import IAR from './IndivAdRight.vue'
 </script>
+-->
 
 <template>
+
+  <div class="iLeft Reveal">
+    <img class="AdImage" src="/src/images/HCJO.png">
+    <div class="Container Reveal">
+      <img class="InsideImage" src="/src/images/HCJO.png">
+      <p class="Title">Fundamentals</p>
+      <p class="Subtitle">We will be teaching HTML, CSS and JS to those without any prior experience in coding. Interested participants may also attend a workshop on Vue, a modern and sleek website development tool.</p>
+    </div>
+  </div>
+
+  <div class="iRight Reveal">
+    <div class="Container Reveal">
+      <img class="InsideImage" src="/src/images/Express.png">
+      <p class="Title">Backend development</p>
+      <p class="Subtitle">Stay tuned for a crash course on managing the backend of your applications with NodeJS, ExpressJS and Firebase.</p>
+    </div>
+    <img class="AdImage" src="/src/images/Express.png">
+  </div>
+
+  <div class="iLeft Reveal">
+    <img class="AdImage" src="/src/images/Vue.png">
+    <div class="Container Reveal">
+      <img class="InsideImage" src="/src/images/Vue.png">
+      <p class="Title">Component-based</p>
+      <p class="Subtitle">Harness the power of Vue.js to make coding your webpage efficient and elegant.</p>
+    </div>
+  </div>
+
+  <div class="iRight Reveal">
+    <div class="Container Reveal">
+      <img class="InsideImage" src="/src/images/Flutter.png">
+      <p class="Title">Mobile App Development</p>
+      <p class="Subtitle">All code written with the Flutter SDK can be deployed anywhere: Windows, Macs, IOS, Android etc. <br>
+    Learn how to use the Flutter SDK to develop cool mobile applications!</p>
+    </div>
+    <img class="AdImage" src="/src/images/Flutter.png">
+  </div>
+
+  <!--
   <p class="Center Title Reveal">Available Workshops</p>
   <IA
     imgsrc="/src/images/HCJO.png"
@@ -23,4 +64,5 @@
     title="Mobile App Development"
     desc="All code written with the Flutter SDK can be deployed anywhere: Windows, Macs, IOS, Android etc.
     Learn how to use the Flutter SDK to develop cool mobile applications!"></IAR>
+  -->
 </template>

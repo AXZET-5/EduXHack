891403c30016217636f4b0b62c7d3e171637e2af
AXZET-5 <51935353+AXZET-5@users.noreply.github.com>
2023-05-06 21:56:38 +0800

    Update TimelineHorizontal.vue

--- src/components/TimelineHorizontal.vue
+++ src/components/TimelineHorizontal.vue
@@ -4,26 +4,26 @@
       <div class="Specialised Reveal">
         <br />
         <text class="Timelined Title">Signups&nbspopen!</text>
-        <p class="Subtitle">4 May 2023</p>
+        <p class="Center Subtitle">4 May 2023</p>
         <text class="Subtitle">/\</text>
       </div>
       <div class="Specialised Reveal">
         <br />
         <text class="Timelined Title">Signups close</text>
-        <p class="Subtitle">17 May 2023</p>
+        <p class="Center Subtitle">17 May 2023</p>
         <text class="Subtitle">/\</text>
       </div>
       <div class="Specialised Reveal">
         <br />
         <text class="Timelined Title">Workshops</text>
-        <p class="Subtitle">19 June 2023</p>
+        <p class="Center Subtitle">19 June 2023</p>
         <text class="Subtitle">/\</text>
         <p class="Subtitle"></p>
       </div>
       <div class="Specialised Reveal">
         <br />
         <text class="Timelined Title">Hackathon!</text>
-        <p class="Subtitle">20-23 June 2023</p>
+        <p class="Center Subtitle">20-23 June 2023</p>
         <p class="Subtitle"></p>
         <text class="Subtitle">/\</text>
       </div>

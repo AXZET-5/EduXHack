6099ab501ab542152ecbf2118436d91d0516c1f5
AXZET-5 <51935353+AXZET-5@users.noreply.github.com>
2023-05-07 11:47:29 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -197,8 +197,8 @@
   }
 
   70% {
-    transform: scale(1);
-    box-shadow: 0 0 0 5px rgba(255, 255, 255, 0);
+    transform: scale(1.1);
+    box-shadow: 0 0 0 10px rgba(255, 255, 255, 0);
   }
 
   100% {
